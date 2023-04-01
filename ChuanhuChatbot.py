e7f19f44a09f65e46811a134d81b074857df315f
izumihoshi <tsunami2deng@gmail.com>
2023-04-01 11:16:05 +0000

    feat: myself

--- ChuanhuChatbot.py
+++ ChuanhuChatbot.py
@@ -343,7 +343,7 @@ with gr.Blocks(css=customCSS, theme=small_and_beautiful_theme) as demo:
 
 logging.info(colorama.Back.GREEN + "\n温馨提示：访问 http://localhost:8000 查看界面" + colorama.Style.RESET_ALL)
 # 默认开启本地服务器，默认可以直接从IP访问，默认不创建公开分享链接
-demo.title = "PCKJ ChatGPT 🚀"
+demo.title = "IZUMI ChatGPT 🚀"
 
 if __name__ == "__main__":
     reload_javascript()
